622c5d1cc402423606bb1b91acb58319dc2f5614
unknown <goluchoudhary7808@gmail.com>
2024-05-25 15:40:10 +0530

    react app with build folder

--- App.js
+++ App.js
@@ -1,20 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { ImageBackground, SafeAreaView, StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const image = { uri: "https://www.canva.com/p/templates/EAFMNm9ybqQ-gold-luxury-initial-circle-logo/"}
+
+const App = () => (
+  <SafeAreaView style={styles.container}>
+   
+      <View style={styles.text}>Elements</View>
+      <Text style={styles.text}>in Front of</Text>
+      <Text style={styles.text}>Background</Text>
+   
+  </SafeAreaView>
+);
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    top:40
+  },
+ 
+  text: {
+    color: 'green',
+    fontSize: 42,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    backgroundColor: 'yellow',
+    borderRadius:10,
   },
 });
+
+export default App;
